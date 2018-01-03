31fcc4b69ef15892b9e540ce3b5cb0c0613d4333
Salvador <salvav1@uci.edu>
2018-01-02 21:02:20 -0800

    ABOUT.HTML: CReating life events change log

---
+++ draft.php
@@ -0,0 +1,16 @@
+<?php
+$a = "SAl";
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>dsfds</title>
+  </head>
+  <body>
+    <?php print($a); ?>
+  </body>
+</html>
